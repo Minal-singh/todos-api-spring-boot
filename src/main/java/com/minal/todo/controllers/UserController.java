9ebccd27c18d32d0dde314c43d66621f3c09af88
Minal Singh <minal@Minals-MacBook-Air.local>
2026-01-22 23:41:35 +0530

    FEATURE: Resolves #3 - Add Global Exception Handler

--- src/main/java/com/minal/todo/controllers/UserController.java
+++ src/main/java/com/minal/todo/controllers/UserController.java
@@ -3,15 +3,12 @@ package com.minal.todo.controllers;
 import com.minal.todo.dto.UserRequestDTO;
 import com.minal.todo.dto.UserResponseDTO;
 import com.minal.todo.dto.UserUpdateDTO;
-import com.minal.todo.exceptions.UserAlreadyExistsException;
-import com.minal.todo.exceptions.UserNotFoundException;
 import com.minal.todo.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -30,41 +27,25 @@ public class UserController {
 
     @GetMapping("/{userName}")
     public ResponseEntity<?> getByUserName(@PathVariable String userName){
-        try {
-            UserResponseDTO user = userService.getUserByUserName(userName);
-            return ResponseEntity.ok(user);
-        } catch (UserNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
-        }
+        UserResponseDTO user = userService.getUserByUserName(userName);
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody UserRequestDTO userDto){
-        try {
-            UserResponseDTO user = userService.createUser(userDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(user);
-        } catch (UserAlreadyExistsException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error", e.getMessage()));
-        }
+        UserResponseDTO user = userService.createUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @PutMapping("/{userName}")
     public ResponseEntity<?> updateUser(@PathVariable String userName, @RequestBody UserUpdateDTO userDto){
-        try {
-            UserResponseDTO user = userService.updateUser(userName, userDto);
-            return ResponseEntity.ok(user);
-        } catch (UserNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
-        }
+        UserResponseDTO user = userService.updateUser(userName, userDto);
+        return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/{userName}")
     public ResponseEntity<?> deleteTodo(@PathVariable String userName){
-        try {
-            userService.deleteUser(userName);
-            return ResponseEntity.noContent().build();
-        } catch (UserNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
-        }
+        userService.deleteUser(userName);
+        return ResponseEntity.noContent().build();
     }
 }
